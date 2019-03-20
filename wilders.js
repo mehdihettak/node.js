44e34e57fa5e292dfe521bcf73175d13cf47f8f5
mehdi hettak <vics06@hotmail.fr>
2019-03-20 11:19:55 +0100

    commit atelier mehdi

---
+++ wilders.js
@@ -0,0 +1,13 @@
+//hello.js
+
+
+const chalk = require('chalk');
+ 
+console.log(chalk.green('davut'));
+console.log(chalk.blue('emile!'));
+console.log(chalk.red('sabrina'));
+console.log(chalk.yellow('damien'));
+
+
+
+
